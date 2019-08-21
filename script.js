83042b614ccec78096d4cae06499f9507dbb0d12
artem-barabash <ivanchik2090@gmail.com>
2019-08-21 10:15:11 +0300

    first-commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("btn-close-1").onclick = function(e) {
+  document.getElementById("datepicker1").value = "";
+}
+
+document.getElementById("btn-close-2").onclick = function(e) {
+  document.getElementById("datepicker2").value = "";
+}
